9d38cc7c1179560a17955599afb7d2bff1fc62c3
moinuddin3188 <moinuddin3188@gmail.com>
2023-10-20 08:24:19 +0600

    first commit

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1";
+  return process.env.NEXT_PUBLIC_API_BASE_URL || "https://jewelery.onrender.com//api/v1";
 };
 
 export const getCloudinaryUrl = (): string => {
