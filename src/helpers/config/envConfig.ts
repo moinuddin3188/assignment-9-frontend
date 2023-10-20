7ede4dd332b7120a258ca06f8fefbe233a2abd7e
moinuddin3188 <moinuddin3188@gmail.com>
2023-10-20 09:50:15 +0600

    Readmi file

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,9 +1,9 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "https://jewelery.onrender.com//api/v1";
+  return process.env.NEXT_PUBLIC_API_BASE_URL || "https://jewelery.onrender.com/api/v1";
 };
 
 export const getCloudinaryUrl = (): string => {
-  
+
   return (
     process.env.CLOUDINARY_API_URL ||
     "https://api.cloudinary.com/v1_1/dezrk5czt/image/upload"
